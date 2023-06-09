84eb0d38fcdb77adfa5a489284b609982f8055c1
Riki <dogisriki1122@gmail.com>
2023-06-09 12:01:20 +0900

    バリデーション機能とメッセージ定義を実装

--- api/src/main/java/com/example/motocatalog/controllers/MotosController.java
+++ api/src/main/java/com/example/motocatalog/controllers/MotosController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,7 +28,11 @@ public class MotosController {
     }
 
     @GetMapping("/motos")
-    public String motos(SearchForm searchForm, Model model) {
+    public String motos(@Validated SearchForm searchForm, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            return "moto_list";
+        }
 
         // バイクメーカードロップダウン取得
         this.setBrands(model);
--- api/src/main/java/com/example/motocatalog/forms/SearchForm.java
+++ api/src/main/java/com/example/motocatalog/forms/SearchForm.java
@@ -1,10 +1,14 @@
 package com.example.motocatalog.forms;
 
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class SearchForm {
 
     private String brandId;
+    
+    @Size(min = 2, max = 10)
     private String keyword;
 }
