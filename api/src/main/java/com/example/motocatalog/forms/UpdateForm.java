a0be79ae6e674a9c673358a7f2f78f8e0862ed91
Riki <dogisriki1122@gmail.com>
2023-06-12 00:12:19 +0900

    更新機能の実装

---
+++ api/src/main/java/com/example/motocatalog/forms/UpdateForm.java
@@ -0,0 +1,23 @@
+package com.example.motocatalog.forms;
+
+import com.example.motocatalog.beans.Brand;
+
+import lombok.Data;
+
+/**
+ * 更新画面に渡す用のForm
+ */
+@Data
+public class UpdateForm {
+
+    private int motoNo;
+    private String motoName;
+    private int seatHeight;
+    private int cylinder;
+    private String cooling;
+    private int price;
+    private String comment;
+    private Brand brand;
+    private int version;
+
+}
--- api/src/main/java/com/example/motocatalog/services/MotosService.java
+++ api/src/main/java/com/example/motocatalog/services/MotosService.java
@@ -1,9 +1,13 @@
 package com.example.motocatalog.services;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.motocatalog.beans.Brand;
 import com.example.motocatalog.beans.Motorcycle;
@@ -21,6 +25,9 @@ public class MotosService {
     @Autowired
     BrandMapper brandMapper;
 
+    @Autowired
+    MessageSource messageSource;
+
     public List<Motorcycle> getMotos(SearchForm form) {
         return motorcycleMapper.selectByCondition(form);
     }
@@ -37,7 +44,25 @@ public class MotosService {
         return brandMapper.selectAll();
     }
 
+    /**
+     * バイク情報更新処理(例外があった場合ロールバックさせる)
+     * 
+     * @param moto
+     * @return 更新件数
+     */
+    @Transactional
     public int save(Motorcycle moto) {
-        return motorcycleMapper.updateByMoto(moto);
+        int updateCount = motorcycleMapper.updateByMoto(moto);
+        // 同時更新があった場合例外をスローする
+        if (updateCount == 0) {
+            throw new OptimisticLockingFailureException(
+                    messageSource.getMessage("error.OptimisticLockingFailureException", null, Locale.JAPANESE));
+        }
+        // 更新後にリダイレクト処理をしているが2件以上の更新があった場合例外をスローする
+        if (updateCount > 1) {
+            throw new RuntimeException(
+                    messageSource.getMessage("An unexpected error has occurred", null, Locale.JAPANESE));
+        }
+        return updateCount;
     }
 }
